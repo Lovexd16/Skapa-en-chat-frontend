cdd4d089c710a71e201c96a992ca14056f307c4c
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-08-28 10:29:25 +0200

    Navigation funktionell
    Även skapat komponenter

---
+++ src/pages/Users.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ShowUsers from "../components/ShowUsers";
+
+function Users() {
+  return (
+    <div>
+      <ShowUsers />
+    </div>
+  );
+}
+
+export default Users;

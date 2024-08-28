9e2505b4f1cbd6bbbc9677ec141a3b9209c38621
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-08-28 13:21:50 +0200

    Fixat med register, login och logout

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,20 @@ import ChatPage from "./pages/ChatPage";
 import UsersPage from "./pages/UsersPage";
 import { useEffect, useState } from "react";
 import Navigation from "./components/Navigation";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   const [page, setPage] = useState<string>("");
+
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(() => {
+    const savedState = localStorage.getItem("isLoggedIn");
+    return savedState ? JSON.parse(savedState) : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn));
+  }, [isLoggedIn]);
 
   useEffect(() => {
     let pageUrl = page;
@@ -19,7 +30,7 @@ function App() {
         pageUrl = getUrl;
         setPage(getUrl);
       } else {
-        pageUrl = "start";
+        pageUrl = "login";
       }
     }
 
@@ -29,13 +40,35 @@ function App() {
   return (
     <>
       <h1>Chatta med vänner</h1>
-      <Navigation setPage={setPage} />
+      <Navigation
+        setPage={setPage}
+        setIsLoggedIn={setIsLoggedIn}
+        isLoggedIn={isLoggedIn}
+      />
 
-      {{
-        start: <StartPage />,
-        chat: <ChatPage />,
-        users: <UsersPage />,
-      }[page] || <StartPage />}
+      {(() => {
+        if (!isLoggedIn) {
+          return (
+            <>
+              {page === "start" && <StartPage />}
+              {page === "login" && (
+                <LoginPage setPage={setPage} setIsLoggedIn={setIsLoggedIn} />
+              )}
+              {page === "register" && <RegisterPage setPage={setPage} />}
+            </>
+          );
+        } else {
+          return (
+            <>
+              {{
+                start: <StartPage />,
+                chat: <ChatPage />,
+                users: <UsersPage />,
+              }[page] || <StartPage />}
+            </>
+          );
+        }
+      })()}
     </>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,28 @@
+import Logout from "./Logout";
+
 interface Props {
   setPage: (page: string) => void;
+  isLoggedIn: boolean;
+  setIsLoggedIn: (loggedIn: boolean) => void;
 }
 
 function Navigation(props: Props) {
   return (
     <div>
       <button onClick={() => props.setPage("start")}>Start</button>
-      <button onClick={() => props.setPage("chat")}>Chatta</button>
-      <button onClick={() => props.setPage("users")}>Användare</button>
+      {props.isLoggedIn && (
+        <>
+          <button onClick={() => props.setPage("chat")}>Chatta</button>
+          <button onClick={() => props.setPage("users")}>Användare</button>
+          <Logout setIsLoggedIn={props.setIsLoggedIn} setPage={props.setPage} />
+        </>
+      )}
+      {!props.isLoggedIn && (
+        <>
+          <button onClick={() => props.setPage("register")}>Registrera</button>
+          <button onClick={() => props.setPage("login")}>Logga in</button>
+        </>
+      )}
     </div>
   );
 }
